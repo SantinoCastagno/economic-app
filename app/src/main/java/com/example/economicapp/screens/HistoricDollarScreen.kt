2e96bdb5e2f6bdde41144e6c7cd9152dc287f2d9
Santino Castagno <santinocastagno.dev@gmail.com>
2022-01-30 15:33:59 -0300

    Worked in historic dollar screen
    Worked with Lazy Column

--- app/src/main/java/com/example/economicapp/screens/HistoricDollarScreen.kt
+++ app/src/main/java/com/example/economicapp/screens/HistoricDollarScreen.kt
@@ -1,6 +1,9 @@
 package com.example.economicapp.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
@@ -14,30 +17,46 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 import androidx.compose.ui.Modifier
+import com.example.economicapp.network.Dollar
 import com.example.economicapp.network.DollarHistorical
 import com.example.economicapp.overview.OverviewViewModel
 
 @Composable
 fun HistoricDollarScreen(navController: NavController, viewModel: OverviewViewModel){
-    val historical: List<DollarHistorical>  by viewModel.statusHistorical().observeAsState(emptyList())
+    val historical: List<DollarHistorical> by viewModel.statusHistorical().observeAsState(emptyList())
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
-                    Text("Valores del mes${historical.size}" )
+                    Text("Valores del mes" ) //${historical[20].valor}
                 }
             ) }
     ){
         // TODO: Diseniar y definir pantalla utilizando "Scaffold","LazyColumn" y "Card"
-        CardWithShape()
+        Column {
+            Text(
+                "El tamaño de la lista es: ${historical.size}"
+            )
+            LazyColumn(
+            ) {
+                items(
+                    items = historical
+                    itemContent = { dollar ->
+                        DollarValueCard()
+                    }
+                ){}
+            }
+        }
     }
 }
 
 @Composable
-fun CardWithShape() {
+fun DollarValueCard() {
     val paddingModifier = Modifier.padding(10.dp)
     Card(shape = RoundedCornerShape(5.dp), modifier = paddingModifier) {
         Text(text = "Round corner shape")
     }
 }
+
+
 
